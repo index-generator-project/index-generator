67e37f56bf77a27bdf8302867d492db7bcd2e7eb
Bruce Zhang <zttt183525594@gmail.com>
2019-06-04 14:24:10 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ from index_generator import APP_NAME, APP_URL, APP_VERSION, PACKAGE_NAME
 setup(
     name=PACKAGE_NAME,
     version=APP_VERSION,
-    packages=['index_generator'],
+    packages=[
+        'index_generator',
+        'index_generator.models'
+    ],
+    package_data={
+        'index_generator': ['templates/*/*']
+    },
+    include_package_data=True,
     url='https://github.com/BruceZhang1993',
     license='MIT',
     author='Bruce Zhang, Edward P',
