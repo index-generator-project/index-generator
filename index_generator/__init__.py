5618ff3ba08ad4470e69271633f1bc126ba8fe20
Bruce Zhang <zttt183525594@gmail.com>
2019-06-04 14:08:26 +0800

    create setup.py

--- index_generator/__init__.py
+++ index_generator/__init__.py
@@ -1,3 +1,4 @@
 APP_NAME = 'Index Generator'
 APP_VERSION = '0.0.1'
 APP_URL = 'https://github.com/BruceZhang1993/index-generator'
+PACKAGE_NAME = 'index-generator'
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from index_generator import APP_NAME, APP_URL, APP_VERSION, PACKAGE_NAME
+
+setup(
+    name=PACKAGE_NAME,
+    version=APP_VERSION,
+    packages=['index_generator'],
+    url='https://github.com/BruceZhang1993',
+    license='MIT',
+    author='Bruce Zhang, Edward P',
+    author_email='',
+    description='Yet another index generator.',
+    install_requires=['Jinja2'],
+    entry_points={
+        'console_scripts': [
+            'index-generator=index_generator.__main__:main'
+        ]
+    }
+)
