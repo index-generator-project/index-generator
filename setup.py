2ec6b3e19ae22f57154b200fd1906178534289cd
Bruce Zhang <zttt183525594@gmail.com>
2019-06-06 22:15:07 +0800

    update setup.py & add travis config

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 from index_generator import APP_NAME, APP_URL, APP_VERSION, PACKAGE_NAME
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = [l for l in f.read().splitlines() if l]
+
 setup(
     name=PACKAGE_NAME,
     version=APP_VERSION,
@@ -17,7 +23,10 @@ setup(
     author='Bruce Zhang, Edward P',
     author_email='',
     description='Yet another index generator.',
-    install_requires=['Jinja2'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['index', 'listing', 'directory'],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'index-generator=index_generator.__main__:main'
