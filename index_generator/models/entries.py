e2bfe17a76d48fc01ba86aacf436d72e7c52fd52
Bruce Zhang <zttt183525594@gmail.com>
2019-06-10 21:37:33 +0800

    add test and format code with pep8

--- index_generator/models/entries.py
+++ index_generator/models/entries.py
@@ -1,8 +1,9 @@
 import os
 import mimetypes
 
+
 def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             if unit == '':
                 return '%3.0f %s%s' % (num, unit, suffix)
@@ -10,6 +11,7 @@ def sizeof_fmt(num, suffix='B'):
                 return '%3.1f %s%s' % (num, unit, suffix)
         num /= 1024.0
     return '%.1f %s%s' % (num, 'Yi', suffix)
+
 
 class Entry(object):
     def __init__(self, file, root, base='/', human=False):
@@ -23,4 +25,3 @@ class Entry(object):
             self.size = os.path.getsize(path)
         self.modified = os.path.getmtime(path)
         self.isDir = os.path.isdir(path)
-    
