bedb6e182a31d1ac9f3ee924f0e515f7c7b1f55b
Bruce Zhang <zttt183525594@gmail.com>
2019-06-19 16:17:25 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
-from index_generator import APP_URL, APP_VERSION, PACKAGE_NAME
+from index_generator import APP_URL, APP_VERSION, PACKAGE_NAME, LICENSE, AUTHOR, EMAIL, DESCRIPTION
 
 with open("README.md", "r") as fh:
     LONG_DESC = fh.read()
 
 with open('requirements.txt') as f:
     REQUIREMENTS = [l for l in f.read().splitlines() if l]
+
+with open('test_requirements.txt') as f:
+    TEST_REQUIREMENTS = [l for l in f.read().splitlines() if l]
 
 setup(
     name=PACKAGE_NAME,
@@ -19,15 +22,16 @@ setup(
     },
     include_package_data=True,
     url=APP_URL,
-    license='MIT',
-    author='Bruce Zhang, Edward P',
-    author_email='',
-    description='Yet another index generator.',
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
     long_description=LONG_DESC,
     long_description_content_type="text/markdown",
     keywords=['index', 'listing', 'directory'],
+    python_requires='>=3.5',
     install_requires=REQUIREMENTS,
-    tests_require=['pytest'],
+    tests_require=TEST_REQUIREMENTS,
     entry_points={
         'console_scripts': [
             'index-generator=index_generator.__main__:main'
